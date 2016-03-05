6e840e686ac56f886dee4a296df7c5b0be02220a
deadlyshot <mttl_daniel@yahoo.com>
2016-03-05 11:58:32 +0200

    Add repository to github

---
+++ SoundAnalyzer/MainUC.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SoundAnalyzer {
+    /// <summary>
+    /// Interaction logic for MainUC.xaml
+    /// </summary>
+    public partial class MainUC : UserControl {
+        MainUCViewModel viewModel = new MainUCViewModel();
+
+        public MainUC() {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
+
+        private void Record_Click(object sender, RoutedEventArgs e) {
+            Console.WriteLine("Recording");
+            viewModel.Record();
+        }
+    }
+}
---
+++ SoundAnalyzer/MainUCViewModel.cs
@@ -0,0 +1,68 @@
+﻿using System.Linq;
+using System.Threading;
+using NAudio.CoreAudioApi;
+using System.Collections.Generic;
+using System;
+using NAudio.Wave;
+
+namespace SoundAnalyzer {
+    class MainUCViewModel : ViewModelBase {
+        private readonly SynchronizationContext synchronizationContext;
+        private MMDevice selectedDevice;
+        private WasapiCapture capture;
+        private float peak;
+
+        public IEnumerable<MMDevice> CaptureDevices { get; private set; }
+
+        public MMDevice SelectedDevice {
+            get { return selectedDevice; }
+            set { if (selectedDevice != value) {
+                    selectedDevice = value;
+                    OnPropertyChanged("SelectedDevice");
+                }
+            }
+        }
+
+        public float Peak {
+            get { return peak; }
+            set { if (peak != value) {
+                    peak = value;
+                    OnPropertyChanged("Peak");
+                }
+            }
+        }
+
+        public MainUCViewModel() {
+            synchronizationContext = SynchronizationContext.Current;
+            var enumerator = new MMDeviceEnumerator();
+            CaptureDevices = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active).ToArray();
+            var defaultDevice = enumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Console);
+            SelectedDevice = CaptureDevices.FirstOrDefault(c => c.ID == defaultDevice.ID);
+        }
+
+        public void Record() {
+            capture = new WasapiCapture(selectedDevice);
+            int i = capture.WaveFormat.ConvertLatencyToByteSize(100);
+            capture.StartRecording();
+            capture.DataAvailable += CaptureOnDataAvabile;
+        }
+
+        int i = 0;
+        private void CaptureOnDataAvabile(object sender, WaveInEventArgs e) {
+            for (int index = 0; index < e.BytesRecorded; index += 2) {
+                short sample = (short)((e.Buffer[index + 1] << 8) |
+                                        e.Buffer[index + 0]);
+                float sample32 = sample / 32768f;
+                i++;
+                Console.WriteLine("I have been summoned" + i + "    " + sample32);
+            }
+            UpdatePeakMeter();
+        }
+
+        void UpdatePeakMeter() {
+            // can't access this on a different thread from the one it was created on, so get back to GUI thread
+            synchronizationContext.Post(s => Peak = SelectedDevice.AudioMeterInformation.MasterPeakValue, null);
+        }
+    }
+}
+

8c52372e9d0df20eda37b842f2783f3b72099654
deadlyshot <mttl_daniel@yahoo.com>
2016-08-19 11:22:16 +0300

    Change Wasapi with WaveIn

--- SoundAnalyzer/ViewModels/MainUCViewModel.cs
+++ SoundAnalyzer/ViewModels/MainUCViewModel.cs
@@ -1,6 +1,5 @@
 ﻿using System.Linq;
 using System.Threading;
-using NAudio.CoreAudioApi;
 using System.Collections.Generic;
 using System;
 using NAudio.Wave;
@@ -10,8 +9,8 @@ using System.Windows.Media;
 namespace SoundAnalyzer.ViewModels {
     class MainUCViewModel : ViewModelBase {
         private readonly SynchronizationContext _synchronizationContext;
-        private MMDevice _selectedDevice;
-        private WasapiCapture _capture;
+        private string _selectedDevice;
+        private WaveIn _waveIn;
         private float _peak;
         private string _line;
         private int _sampleRate;
@@ -22,14 +21,18 @@ namespace SoundAnalyzer.ViewModels {
         private string _path;
         private int _sensibility;
 
-        public IEnumerable<MMDevice> CaptureDevices { get; private set; }
+        public IEnumerable<string> CaptureDevices { get; private set; }
+        public List<WaveInCapabilities> CaptureDevicesMetadata { get; private set; }
         public double TargetFreaquency { get; set; }
         public double Power { get; set; }
 
 
-        public MMDevice SelectedDevice {
+        public string SelectedDevice {
             get { return _selectedDevice; }
-            set { if (_selectedDevice != value) { _selectedDevice = value; OnPropertyChanged("SelectedDevice"); GetSampleRate(value); } }
+            set {
+                _selectedDevice = value; OnPropertyChanged("SelectedDevice");
+                GetSampleRate(value); Record();
+            }
         }
 
         public float Peak {
@@ -67,11 +70,23 @@ namespace SoundAnalyzer.ViewModels {
             _synchronizationContext = SynchronizationContext.Current;
             AddAllNotes();
             InitializeNotes();
-            var enumerator = new MMDeviceEnumerator();
-            CaptureDevices = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active).ToArray();
-            var defaultDevice = enumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Console);
-            SelectedDevice = CaptureDevices.FirstOrDefault(c => c.ID == defaultDevice.ID);
-            Record();
+            GetRecordingDevices();
+
+        }
+
+        private void GetRecordingDevices() {
+            //var enumerator = new MMDeviceEnumerator();
+            //CaptureDevices = enumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active).ToArray();
+            //var defaultDevice = enumerator.GetDefaultAudioEndpoint(DataFlow.Capture, Role.Console);
+            //SelectedDevice = CaptureDevices.FirstOrDefault(c => c.ID == defaultDevice.ID);
+            var waveInDevices = WaveIn.DeviceCount;
+            CaptureDevicesMetadata = new List<WaveInCapabilities>();
+            for (var waveInDevice = 0; waveInDevice < waveInDevices; waveInDevice++) {
+                var deviceInfo = WaveIn.GetCapabilities(waveInDevice);
+                CaptureDevicesMetadata.Add(deviceInfo);
+            }
+            CaptureDevices = CaptureDevicesMetadata.Select(d => d.ProductName);
+            SelectedDevice = CaptureDevices.First();
         }
 
         private void InitializeNotes() {
@@ -97,22 +112,26 @@ namespace SoundAnalyzer.ViewModels {
         }
 
         public void Record() {
-            _capture = new WasapiCapture(_selectedDevice);
-            WaveFormat myformat = new WaveFormat(44100, 16, 2);
-            _capture.WaveFormat = myformat;
-            _capture.DataAvailable += CaptureOnDataAvabile;
-            _capture.StartRecording();
+            _waveIn = new WaveIn();
+            var captureDevice = CaptureDevicesMetadata.Find(c => c.ProductName == SelectedDevice);
+            _waveIn.DeviceNumber = CaptureDevicesMetadata.IndexOf(captureDevice);
+            //WaveFormat myformat = new WaveFormat(44100, 16, 2);
+            //_capture.WaveFormat = myformat;
+            _waveIn.DataAvailable += CaptureOnDataAvabile;
+            _waveIn.StartRecording();
         }
 
-        private void GetSampleRate(MMDevice value) {
-            using (var c = new WasapiCapture(value)) {
-                _sampleRate = c.WaveFormat.SampleRate;
+        private void GetSampleRate(string microphoneName) {
+            using (var capture = new WaveIn()) {
+                var captureDevice = CaptureDevicesMetadata.Find(c => c.ProductName == microphoneName);
+                capture.DeviceNumber = CaptureDevicesMetadata.IndexOf(captureDevice);
+                _sampleRate = capture.WaveFormat.SampleRate;
                 _goertzelFilter = new GoertzelAlgorithm(_sampleRate);
             }
         }
 
         private void CaptureOnDataAvabile(object sender, WaveInEventArgs e) {
-            UpdatePeakMeter();
+            //UpdatePeakMeter();
             var buffer = new List<float>();
             for (int index = 0; index < e.BytesRecorded; index += 2) {
                 short sample = BitConverter.ToInt16(e.Buffer, index);
@@ -120,7 +139,14 @@ namespace SoundAnalyzer.ViewModels {
                 buffer.Add(sample32);
             }
 
-            if (_goertzelFilter.NotesPlayed(buffer, _sheet.GetCurentNotes())) {
+            //if (_goertzelFilter.NotesPlayed(buffer, _sheet.GetCurentNotes())) {
+            //    ChangeNotesColor();
+            //    _sheet.IncreaseCounter();
+            //    Line = _sheet.GetCurrentNotesAsString();
+            //}
+
+            Keys = _goertzelFilter.DetectAllNotesPlayed(buffer);
+            if (_sheet.GetCurentNotes().Select(n => n.ToString()).All(key => Keys[key] == 1)) {
                 ChangeNotesColor();
                 _sheet.IncreaseCounter();
                 Line = _sheet.GetCurrentNotesAsString();
@@ -133,8 +159,8 @@ namespace SoundAnalyzer.ViewModels {
             _synchronizationContext.Post(s => OnPropertyChanged("Notes"), null);
         }
 
-        private void UpdatePeakMeter() {
-            _synchronizationContext.Post(s => Peak = SelectedDevice.AudioMeterInformation.MasterPeakValue, null);
-        }
+        //private void UpdatePeakMeter() {
+        //    _synchronizationContext.Post(s => Peak = SelectedDevice.AudioMeterInformation.MasterPeakValue, null);
+        //}
     }
 }
